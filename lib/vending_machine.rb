0f757fec8fb58c7a5df693795d87224fc9c25a49
Sam Joseph <tansaku@gmail.com>
2015-06-10 13:48:46 +0100

    fixes self etc.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -24,7 +24,7 @@ class VendingMachine
 
   def insert coin
     if VALID_COINS.include? coin
-      self.coins.push(coin)
+      coins.push(coin)
       self.display = "#{total} cents"
     else
       self.coin_return = coin
@@ -37,15 +37,13 @@ class VendingMachine
     vend
   end
 
-  protected
+  private
 
   attr_reader :coins,
               :product,
               :ready_to_reset,
               :ready_to_insufficient_payment_reset,
               :payment_sufficient
-
-  private
 
   attr_writer :display,
               :coin_return,
@@ -62,8 +60,8 @@ class VendingMachine
   end
 
   def vend
-    return unless self.product
-    dispense_product 
+    return unless product
+    dispense_product
     make_change
     update_display
   end
@@ -73,17 +71,17 @@ class VendingMachine
   end
 
   def dispense_product
-    self.hopper = self.product if payment_sufficient?
+    self.hopper = product if payment_sufficient?
   end
 
   def make_change
-    return if total <= self.product.price
+    return if total <= product.price
     coins.sort! {|a,b| b.to_i <=> a.to_i}
     coin_values = coins.map(&:to_i)
     remainder = 0
     used_coins = []
     coin_values.each do |coin|
-      remainder = self.product.price - (used_coins.inject(:+) || 0) - coin
+      remainder = product.price - (used_coins.inject(:+) || 0) - coin
       break if remainder < 0
       used_coins << coin
     end
@@ -96,15 +94,15 @@ class VendingMachine
   end
 
   def total
-    self.coins.map(&:to_i).inject(:+) || 0
+    coins.map(&:to_i).inject(:+) || 0
   end
 
   def payment_sufficient?
-    @payment_sufficient ||= total >= self.product.price
+    @payment_sufficient ||= total >= product.price
   end
 
   def handle_insufficient_payment_state
-    if self.ready_to_insufficient_payment_reset
+    if ready_to_insufficient_payment_reset
       self.display = total > 0 ? "#{total} cents" : 'INSERT COIN'
       self.ready_to_insufficient_payment_reset = false
     end
@@ -112,7 +110,7 @@ class VendingMachine
   end
 
   def handle_purchase_completed_state
-    initialize if self.ready_to_reset
+    initialize if ready_to_reset
     self.ready_to_reset = true if @display == 'THANK YOU'
   end
 end
